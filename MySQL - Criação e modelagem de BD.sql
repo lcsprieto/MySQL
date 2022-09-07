3b54b0d4caa29be14b9fec134d4b1a002929e7ca
lcsprieto <103700100+lcsprieto@users.noreply.github.com>
2022-09-07 11:55:52 -0300

    Update MySQL - Criação e modelagem de BD.sql

--- MySQL - Criação e modelagem de BD.sql
+++ MySQL - Criação e modelagem de BD.sql
@@ -4,14 +4,14 @@ create database bella DEFAULT CHARSET = utf8mb4;
 use bella;
 -- criando primeira tabela, "pacientes"
 create table pacientes(
-			CPF varchar(14) not null,
-            Nome varchar(30) not null,
-            Telefone varchar(14),
-            Email varchar(30),
-            Endereço varchar(30),
-			Bairro varchar(20),
-            Cidade varchar (20) default 'Curitiba',
-            UF char (2) DEFAULT 'PR'
+	CPF varchar(14) not null,
+        Nome varchar(30) not null,
+        Telefone varchar(14),
+        Email varchar(30),
+        Endereço varchar(30),
+	Bairro varchar(20),
+        Cidade varchar (20) default 'Curitiba',
+        UF char (2) DEFAULT 'PR'
            );
 start transaction; -- opcional, para desabilitar autocommit e evitar atomicidade das alterações
 insert into pacientes (CPF, Nome, Telefone, Email, Endereço, Bairro, Cidade, UF) values ('345.567.890-00', 'Maria da Silva', '41 98765 8456', 'msilva@gmail.com', 'Rua das Margaridas, 33', 'Pilarzinho', 'Curitiba', 'PR'), ('123.654.987-11', 'Ana do Santos', '41 98990 1234', 'anasantos@gmail.com', 'Rua Alegria, 234', 'Santo Inácio', 'Curitiba', 'PR'), ('321.543.654-22', 'Ana dos Santos', '41 98423 5456', 'ana_dos_santos@hotmail.com', 'Rua 21 de Abril, 1430', 'Alto da XV', 'Curitiba', 'PR');
